45d02fb8b34fbe3466015931b439cc60f5f57791
lishen-razer <hljlishen@hotmail.com>
2019-07-16 14:46:41 +0800

    添加Tools项目，和ExpressionBuilder类

---
+++ Tools/ExpressionBuilder/ReplaceParamVisitor.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+
+namespace Tools
+{
+    internal class ReplaceParamVisitor : ExpressionVisitor
+    {
+        public ReplaceParamVisitor(ParameterExpression param)
+        {
+            this.param = param;
+        }
+
+        public ParameterExpression param { get; private set; }
+
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            return param;
+        }
+
+    }
+}
