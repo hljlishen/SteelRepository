bc750c332ed1174a0ea899b61eb4233693e88660
lishen-razer <hljlishen@hotmail.com>
2019-07-16 14:39:56 +0800

    添加DbInterface项目

---
+++ DbInterface/DbInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace DbInterface
+{
+    interface DbInterface
+    {
+        int Insert<T>(T t);
+        List<T> Select<T>(Expression<Func<T, bool>> expression);
+        T FindId<T>(int id);
+        int Update<T>(T t);
+        int Delete<T>(int id);
+        int DeleteWhere<T>(Expression<Func<T, bool>> expression);
+    }
+}
