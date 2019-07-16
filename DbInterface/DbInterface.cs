4b0bda5c75aacfd513d783ce8b39a663ce9949c2
lishen-razer <hljlishen@hotmail.com>
2019-07-16 16:14:01 +0800

    修改接口DbInterface

--- DbInterface/DbInterface.cs
+++ DbInterface/DbInterface.cs
@@ -4,13 +4,11 @@ using System.Linq.Expressions;
 
 namespace DbInterface
 {
-    interface DbInterface
+    interface IDbInterface
     {
         int Insert<T>(T t);
         List<T> Select<T>(Expression<Func<T, bool>> expression);
-        T FindId<T>(int id);
         int Update<T>(T t);
-        int Delete<T>(int id);
         int DeleteWhere<T>(Expression<Func<T, bool>> expression);
     }
 }
