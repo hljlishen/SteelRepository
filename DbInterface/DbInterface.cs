5c01a8a880af64e02d4eab1b2770730fc519edcb
lishen-razer <hljlishen@hotmail.com>
2019-07-16 20:28:41 +0800

    修改DbInterface接口函数签名

--- DbInterface/DbInterface.cs
+++ DbInterface/DbInterface.cs
@@ -8,14 +8,15 @@ namespace DbInterface
     public interface IDbInterface : IDisposable
     {
         int Insert<T>(T t, bool shouldCommit = true) where T : class;
-        int Insert<T>(IEnumerable<T> tList, bool shouldCommit = true) where T : class;
+        int InsertRange<T>(IEnumerable<T> tList, bool shouldCommit = true) where T : class;
         T FindId<T>(int id) where T : class;
         IQueryable<T> Select<T>(Expression<Func<T, bool>> expression) where T : class;
+        IQueryable<T> SelectAll<T>() where T : class;
         IQueryable<T> SelectPage<T>(Expression<Func<T, bool>> expression, int pageSize, int pageNum, Expression<Func<T, bool>> orderExp = null, bool isAsc = true) where T : class;
         int Update<T>(T t, bool shouldCommit = true) where T : class;
         int Delete<T>(T t, bool shouldCommit = true) where T : class;
         int Delete<T>(int id, bool shouldCommit = true) where T : class;
-        int Delete<T>(IEnumerable<T> tList, bool shouldCommit = true) where T : class;
+        int DeleteRange<T>(IEnumerable<T> tList, bool shouldCommit = true) where T : class;
         int DeleteWhere<T>(Expression<Func<T, bool>> expression, bool shouldCommit = true) where T : class;
         int Commit();
     }
