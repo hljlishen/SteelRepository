82fec0a138124c71f4800ae7012c973d31f51a8e
lishen-razer <hljlishen@hotmail.com>
2019-07-18 18:36:23 +0800

    更改Dbinterface接口

--- DbInterface/DbInterface.cs
+++ DbInterface/DbInterface.cs
@@ -7,17 +7,18 @@ namespace DbInterface
 {
     public interface IDbInterface : IDisposable
     {
-        int Insert<T>(T t, bool shouldCommit = true) where T : class;
-        int InsertRange<T>(IEnumerable<T> tList, bool shouldCommit = true) where T : class;
+        bool InstantCommittion { get; set; }
+        int Insert<T>(T t) where T : class;
+        int InsertRange<T>(IEnumerable<T> tList) where T : class;
         T FindId<T>(int id) where T : class;
         IQueryable<T> Select<T>(Expression<Func<T, bool>> expression) where T : class;
         IQueryable<T> SelectAll<T>() where T : class;
         IQueryable<T> SelectPage<T>(Expression<Func<T, bool>> expression, int pageSize, int pageNum, Expression<Func<T, bool>> orderExp = null, bool isAsc = true) where T : class;
-        int Update<T>(T t, bool shouldCommit = true) where T : class;
-        int Delete<T>(T t, bool shouldCommit = true) where T : class;
-        int Delete<T>(int id, bool shouldCommit = true) where T : class;
-        int DeleteRange<T>(IEnumerable<T> tList, bool shouldCommit = true) where T : class;
-        int DeleteWhere<T>(Expression<Func<T, bool>> expression, bool shouldCommit = true) where T : class;
+        int Update<T>(T t) where T : class;
+        int Delete<T>(T t) where T : class;
+        int Delete<T>(int id) where T : class;
+        int DeleteRange<T>(IEnumerable<T> tList) where T : class;
+        int DeleteWhere<T>(Expression<Func<T, bool>> expression) where T : class;
         int Commit();
     }
 }
