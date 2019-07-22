53aec4e0f6aec926d59f35cdc3e427f01008573e
mushiliyj <mushilinyj@outlook.com>
2019-07-22 16:40:36 +0800

    ExpressionBuilder测试用例ExpressionBuilderTests

--- ToolsTests/ExpressionBuilder/ExpressionBuilderTests.cs
+++ ToolsTests/ExpressionBuilder/ExpressionBuilderTests.cs
@@ -20,23 +20,49 @@ namespace Tools.Tests
         [TestMethod()]
         public void AndTest()
         {
+            List<int?> ls = new List<int?>();
+            ls.Add(11);
+            ls.Add(3);
+            ls.Add(1);
+            ls.Add(4); 
+            ls.Add(5);
+            ls.Add(6);
+            ls.Add(12);
+            ls.Add(16);
+            ls.Add(17);
             ExpressionBuilder<int?> builder = new ExpressionBuilder<int?>();
             builder.And(p => p.Value > 5);
             builder.And(p => p.Value < 15);
-
             var exp = builder.GetExpression();
+            //string expStr = exp.ToString();
+            var result = ls.Where(exp.Compile());
+            Assert.AreEqual(result.Count(), 3);
         }
-
         [TestMethod()]
         public void OrTest()
         {
-            Assert.Fail();
+            List<int?> ls = new List<int?>();
+            ls.Add(11);
+            ls.Add(3);
+            ls.Add(1);
+            ls.Add(4);
+            ls.Add(5);
+            ls.Add(6);
+            ls.Add(12);
+            ls.Add(16);
+            ls.Add(17);
+            ExpressionBuilder<int?> builder = new ExpressionBuilder<int?>();
+            builder.Or(p => p.Value>7);
+            builder.Or(p => p.Value==3);
+            var exp = builder.GetExpression();
+            var result = ls.Where(exp.Compile());
+            Assert.AreEqual(result.Count(), 5);
         }
 
         [TestMethod()]
         public void GetExpressionTest()
         {
-            Assert.Fail();
+            ExpressionBuilder<int?> builder = new ExpressionBuilder<int?>();
         }
     }
 }
