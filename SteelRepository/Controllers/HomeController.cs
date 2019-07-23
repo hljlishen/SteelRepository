9eed7a9318d19895c070d13beea528e04fbdb12e
lishen-razer <hljlishen@hotmail.com>
2019-07-23 21:43:21 +0800

    删除多余文件

--- SteelRepository/Controllers/HomeController.cs
+++ SteelRepository/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SteelRepository.Models;
 
 namespace SteelRepository.Controllers
 {
@@ -10,6 +11,7 @@ namespace SteelRepository.Controllers
     {
         public ActionResult Index()
         {
+            InCome inc = new InCome();
             return View();
         }
 
