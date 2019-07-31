2752fc3f2a469bd771e6a8ec48b7efca5906a007
华 马 <m17791843428@hotmail.com>
2019-07-31 16:55:29 +0800

    项目管理页面 7-31

--- SteelRepository/App_Start/RouteConfig.cs
+++ SteelRepository/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SteelRepository
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Login", action = "AdminIndex", id = UrlParameter.Optional }
+                defaults: new { controller = "Project", action = "Project_list", id = UrlParameter.Optional }
             );
         }
     }
