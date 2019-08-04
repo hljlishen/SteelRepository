92eff20615816f6b03c13f57fc142c6e9d10a24a
katherinetianyu@163.com <katherinetianyu@163.com>
2019-08-04 23:40:49 +0800

    厂商0804

---
+++ SteelRepository/Controllers/ManufacturerController.cs
@@ -0,0 +1,25 @@
+﻿using DbInterface;
+using DbService;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SteelRepository.Controllers
+{
+    public class ManufacturerController : Controller
+    {
+        // GET: Manufacturer
+        public ActionResult Manufacturer_add()
+        {
+            return View();
+        }
+        public ActionResult Manufacturer_list()
+        {
+            return View();
+        }
+        
+    }
+}
