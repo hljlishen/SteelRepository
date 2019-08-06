479be5771b305b06cf1a80a2cc240fcc666c82e2
katherinetian <katherinetianyu@163.com>
2019-08-06 18:02:22 +0800

    8-6

--- SteelRepository/Controllers/ManufacturerController.cs
+++ SteelRepository/Controllers/ManufacturerController.cs
@@ -2,6 +2,7 @@
 using DbService;
 using Models;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,15 +12,68 @@ namespace SteelRepository.Controllers
 {
     public class ManufacturerController : Controller
     {
-        // GET: Manufacturer
+        private static Manufacturer mf;
+        // GET: Employee
+        public ActionResult Manufacturer_list()
+        {
+            return View(Manufacturer.SelectAll());
+        }
+
         public ActionResult Manufacturer_add()
         {
             return View();
         }
-        public ActionResult Manufacturer_list()
+
+        [HttpPost]
+        public JsonResult Manufacturer_add(Manufacturer manufacturer)
         {
+            return Json(Manufacturer.Insert(manufacturer));
+        }
+
+        public ActionResult Manufacturer_update(int id)
+        {
+            ViewData["Manufacturer"] = Manufacturer.GetManufacturer(id);
+            mf = Manufacturer.GetManufacturer(id);
             return View();
         }
         
+        [HttpPost]
+        public JsonResult Manufacturer_update(Manufacturer manufacturer)
+        {
+            manufacturer.id = mf.id;
+            manufacturer.manufacturersName = mf.manufacturersName;
+            manufacturer.contact = mf.contact;
+            manufacturer.address = mf.address;
+            return Json(Manufacturer.Update(manufacturer));
+        }
+
+        public ActionResult Manufacturer_select(int id)
+        {
+            ViewData["Manufacturer"] = Manufacturer.GetManufacturer(id);
+            return View();
+        }
+
+        public JsonResult Manufacturer_delete(int id)
+        {
+            return Json(Manufacturer.Delete(id));
+        }
+
+        public ActionResult Manufacturer_information(int id)
+        {
+            Manufacturer manufacturer = Manufacturer.GetManufacturer(id);
+            ViewData["Manufacturer"] = manufacturer;
+            mf = manufacturer;
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Manufacturer_information(Manufacturer manufacturer)
+        {
+            manufacturer.id = mf.id;
+            manufacturer.manufacturersName = mf.manufacturersName;
+            manufacturer.contact = mf.contact;
+            manufacturer.address = mf.address;
+            return Json(Manufacturer.Update(manufacturer));
+        }
     }
 }
