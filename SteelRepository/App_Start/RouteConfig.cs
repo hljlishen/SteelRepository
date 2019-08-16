e59319f9269e68789120a056fefd07130dc23f03
华 马 <m17791843428@hotmail.com>
2019-08-16 15:15:32 +0800

    更换主页面完成登录、注册

--- SteelRepository/App_Start/RouteConfig.cs
+++ SteelRepository/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SteelRepository
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Inventory", action = "Inventory", id = UrlParameter.Optional }
+                defaults: new { controller = "Index", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
